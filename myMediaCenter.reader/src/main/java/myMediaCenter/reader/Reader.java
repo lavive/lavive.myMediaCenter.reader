4e8eabddf9bfa3c0efde6e009cd2d5b5fa8e1fea
root <root@vivien-All-Series>
2019-01-23 12:10:07 +0100

    Initial Commit

---
+++ myMediaCenter.reader/src/main/java/myMediaCenter/reader/Reader.java
@@ -0,0 +1,18 @@
+package myMediaCenter.reader;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Reader extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+        Application.launch(Reader.class, args);
+	}
+
+}
---
+++ myMediaCenter.reader/src/main/java/myMediaCenter/reader/view/WindowController.java
@@ -0,0 +1,16 @@
+package myMediaCenter.reader.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class WindowController implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
